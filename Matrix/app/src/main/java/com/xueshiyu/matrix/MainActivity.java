fe752e981fd9f84e9a5918f13a97e94ddc47d2dd
Shiyu Xue <shiyuxue@usc.edu>
2018-10-23 09:52:29 -0700

    full version of Matrix

---
+++ Matrix/app/src/main/java/com/xueshiyu/matrix/MainActivity.java
@@ -0,0 +1,76 @@
+package com.xueshiyu.matrix;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity implements EventFragment.OnItemSelectListener, CommentFragment.OnItemSelectListener {
+
+    @Override
+    public void onItemSelected(int position){
+        if (!isTablet()) {
+            Intent intent = new Intent(this, EventGridActivity.class);
+            intent.putExtra("position", position);
+            startActivity(intent);
+        } else {
+            mGridFragment.onItemSelected(position);
+        }
+
+    }
+
+    @Override
+    public void onCommentSelected(int position) {
+        mListFragment.onItemSelected(position);
+    }
+
+
+
+    private EventFragment mListFragment;
+    private CommentFragment mGridFragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //add list view
+        mListFragment = new EventFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.event_container, mListFragment).commit();
+
+
+        //add Gridview
+        if (isTablet()) {
+            mGridFragment = new CommentFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.comment_container, mGridFragment).commit();
+
+        }
+    }
+
+    private boolean isTablet() {
+        return (getApplicationContext().getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) >=
+                Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+
+
+
+
+
+    /**
+     * A dummy function to get fake event names.
+     */
+    private String[] getEventNames() {
+        String[] names = {
+                "Event1", "Event2", "Event3",
+                "Event4", "Event5", "Event6",
+                "Event7", "Event8", "Event9",
+                "Event10", "Event11", "Event12"};
+        return names;
+    }
+}
---
+++ Matrix/app/src/main/java/com/xueshiyu/matrix/MyFirebaseInstanceIDService.java
@@ -0,0 +1,18 @@
+package com.xueshiyu.matrix;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.support.constraint.Constraints.TAG;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+    }
+
+}
